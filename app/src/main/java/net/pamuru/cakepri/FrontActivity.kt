a124fb6c72f2fd4a79db7dd6e4e28985e668749f
MarlanHR <marlan777@users.noreply.github.com>
2018-07-07 09:23:50 +0800

    20180707 Add front image

--- app/src/main/java/net/pamuru/cakepri/FrontActivity.kt
+++ app/src/main/java/net/pamuru/cakepri/FrontActivity.kt
@@ -3,10 +3,10 @@ package net.pamuru.cakepri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FrontActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_Front)
     }
 }
